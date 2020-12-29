3be90f9a3d8fe4da81b8b7c0e41a5521fa0aaaa7
KatherineHuangg <61874435+KatherineHuangg@users.noreply.github.com>
2020-12-29 23:22:25 +0800

    Delete close.php

--- data/close.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (strtolower($message['text']) == "start") {
-    $sth = $connect->prepare('UPDATE line_state SET line_state = :hp WHERE line_id = 1');
-    $sth->bindValue(':hp','start');
-    $sth->execute();
-}
